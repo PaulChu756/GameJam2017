e940fb9ae3c0c7fd9acb7254dfdd42b7fbb7b475
unknown <lordbabypanda@gmail.com>
2017-11-11 19:16:26 -0600

    added simple controllers

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float jumpSpeed = 10;
+    public float movementSpeed = 10; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        checkInput();
+	}
+
+    void checkInput()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("space key is pressed");
+            transform.position += transform.up * jumpSpeed * Time.deltaTime;
+        }
+
+        if(Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("D is pressed");
+            transform.position += transform.right * movementSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("A is pressed");
+            transform.position += -transform.right * movementSpeed * Time.deltaTime;
+        }
+    }
+}
